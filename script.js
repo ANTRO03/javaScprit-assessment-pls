9b8af14836311978cf052a19b4f2a1733b989606
Tristan Ramos <tristanramos543@gmail.com>
2023-02-04 14:58:12 -0500

    some message here

---
+++ script.js
@@ -0,0 +1,12 @@
+const firstName = "Tristan"
+const lastName = "Ramos"
+const age = "19"
+
+console.log("I am having alot of fun learning code in this class")
+
+alert(" My name is " + firstName + " " + lastName)
+
+alert(" I am " +  age  + " years old ")
+
+
+
